683ab34e3e597d3794ba25186126074e0d93723d
Madi <wmadi20@yahoo.com>
2019-02-01 15:41:24 -0600

    finished challenge

---
+++ Expenses/Expense.swift
@@ -0,0 +1,15 @@
+//
+//  Expense.swift
+//  Expenses
+//
+//  Created by Madison Williams on 1/30/19.
+//  Copyright © 2019 Madison Williams. All rights reserved.
+//
+
+import Foundation
+
+struct Expense{
+    let title: String
+    let amount: Double
+    let date: Date
+}
